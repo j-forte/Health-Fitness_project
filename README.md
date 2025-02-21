7cece4800990a8405f2b7cfd3eb1bdf47c8d17a1
James Forte <jcfforte@gmail.com>
2025-02-21 17:43:52 -0500

    Working through adding functions and EDA charts

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Health-Fitness_project
 Health and Fitness EDA
+
+Main Goals of the Project
+Your project should aim to:
+
+Key Components of a Successful Project
+    Effective presentation
+    Clear README
+    Well-organized code and GitHub repository
+
+Pose a research question or state an objective
+    Source relevant data
+    Explore the data
+    Clean the data
+    Analyze the data
+    Present key findings
+    Offer recommendations
+    Suggest future research areas
---
+++ function.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+
+
+'''
+Write a function that will take in a datafram and a participant_id and return a datafram with only the participant data
+'''
+def set_up_participants_df(df, participant_id):
+    if isinstance(df, pd.DataFrame):
+        if 'participant_id' in df.columns:
+            participant_data = df[df['participant_id'] == participant_id]
+            return participant_data
+    else:
+        raise ValueError("The input is not a valid DataFrame.")
+    
+
+'''
+Writa a funciton that will get the z-score of a column in a dataframe
+'''
+def outlier_cleaner(feature):
+    df_q1 = new_df_data[feature].quantile(.25)
+    df_q3 = new_df_data[feature].quantile(.75)
+    df_med = new_df_data[feature].quantile(.50)
+    iqr = df_q3 - df_q1
+    lower = df_q1 - iqr * 1.5
+    upper = df_q3 + iqr * 1.5
+    no_outliers_df = new_df_data[(new_df_data[feature] >= lower) & (new_df_data[feature] <= upper)]
+    return no_outliers_df
